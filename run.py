a99e92eada9d7e895743a960388c35d3c2133e4f
bbonnuss <bbonnuss1@hotmail.com>
2019-04-09 13:03:00 +0700

    update

--- run.py
+++ run.py
@@ -7,34 +7,52 @@ from os.path import join
 
 
 # Class ========================== Class ========================== Class
+# Menu --------------------------- Menu
 class Scene():
     def __init__():
         self.inprocess = True
         self.click_pos = pygame.mouse.get_pos()
         self.blackground = None
+        self.theme_song = None
 
-
+# main menu
 class Main_menu(Scene):
     Scene.__init__(self)
 
-
+# shop
 class Shop_menu(Scene):
     Scene.__init__(self)
 
-
+# คลัง
 class Storage_menu(Scene):
     Scene.__init__(self)
 
-
-class Achievement(Scene):
+# Achievement
+class Achievement_manu(Scene):
     Scene.__init__(self)
 
+# แปรรูป
+class Process_menu(Scene):
 
-class 
+
+# Player ------------------------- Menu
+class Player():
+    def __init__(self):
+        self.money = 0
+        self.player_farm = None
+
+class Farmland():
+    def __init__():
+        self.size = 4
+        
+# mechanic class
+class Plant():
+    def __init__(self):
+        self.
 
 # Launcher ======================= Launcher ======================= Launcher 
 pygame.init()
-resolution = (720,480) # ED
+resolution = (720,480)
 game_window = pygame.display.set_mode(resolution)
 pygame.display.set_caption("Cute, Ginger, Cat-ting Stealing  Vegetables ")
 
